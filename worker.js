b3f48c42922dbe8e4c1d23a5d2cffe1382b960e6
vip-weizhen <47912513+vip-weizhen@users.noreply.github.com>
2021-05-13 15:49:26 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -353,7 +353,7 @@ function renderHTML(index,seller) {
       $('#sengine a').on('click', function (e) {
         $('#sengine a.active').toggleClass('active');
         $(e.target).toggleClass('active');
-        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + '/favicon.ico') ;
+        $('#search-fav').attr('src',$(e.target).data('url').match(`+/https{0,1}:\/\/\S+\//+`)[0] + 'favicon.ico') ;
       });
       $('.search').on('click', function (e) {
           var url = $('#sengine a.active').data('url');
